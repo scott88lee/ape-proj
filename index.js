6d68b6fe3e5b6bc40a1166aae9fcf1c63bc33882
scott88lee@gmail.com <scott88lee@gmail.com>
2022-01-30 00:22:30 +0800

    proj mvc structure

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const db = require('.model/db');
 
-app.use('/', express.static('public'));
+app.use('/', express.static('view'));
 
 app.get('/hello', (req, res) => {
+    let exc = db.read('db.json');
+    console.log(exc);
     res.send('Hello');
 })
 
-app.listen(8080, ()=>{console.log("Server running: Port 8080")})
+app.get('/goodbye', (req, res) => {
+    let exc = db.read('db.json');
+    exc.happines = "MAXIMUM";
+    db.write('db.json', exc);
+    res.send('Goodbye');
+})
+
+app.listen(8080, () => {console.log("Server running: Port 8080")})
