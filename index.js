fff9d4ef9ae2d3b40ef1f4228968dfab1056eb29
scott88lee@gmail.com <scott88lee@gmail.com>
2022-01-30 12:50:36 +0800

    init db

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const db = require('.model/db');
+const db = require('./model/db');
 
 app.use('/', express.static('view'));
 
@@ -15,6 +15,11 @@ app.get('/goodbye', (req, res) => {
     exc.happines = "MAXIMUM";
     db.write('db.json', exc);
     res.send('Goodbye');
+})
+
+app.get('/api', (req, res) => {
+    let exc = db.read('db.json');
+    res.send(exc);
 })
 
 app.listen(8080, () => {console.log("Server running: Port 8080")})
