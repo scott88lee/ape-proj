801d48ce9fa5c4457b1f3c9f8629f06dcbd64e4f
scott88lee@gmail.com <scott88lee@gmail.com>
2022-02-19 13:53:00 +0800

    No more errors

--- index.js
+++ index.js
@@ -1,65 +1,24 @@
 const express = require('express');
 const app = express();
-const db = require('./model/db');
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use('/', express.static('view'));
 
-app.get('/api/excavators', (req, res) => {
-    let data = db.read('db.json');
-    res.send(data);
-})
+const ctl = require('./controller/main');
 
-app.post('/api/excavators', (req, res) => {
-    let data = db.read('db.json');
-    if (data.excavators.length < 6) {
-        data.excavators.push(
-            {
-                id: data.excavators.length + 1,
-                name: req.body.name,
-                description: req.body.description
-            }
-        )
-    }
-    db.write('db.json', data)
-    res.redirect("/excavator.html");
-})
+//API Routes
+app.get('/api/excavators', ctl.getAllExcavators);
+// app.post('/api/excavators', ctl.addExcavator);
+// app.get('/api/delete/excavators/:id', ctl.deleteExcavator);
 
-app.get('/api/delete/excavators/:id', (req, res) => {
-    let id = req.params.id;
-    let data = db.read('db.json');
-    data.excavators.splice(id - 1, 1);
-    db.write('db.json', data);
-    res.redirect("/excavator.html");
-})
+// app.get('/api/schedules', ctl.getAllSchedules);
 
-app.get('/api/schedules', (req, res) => {
-    let data = db.read('db.json');
-    let schedule = [];
-
-    for (let i=0; i<data.excavators.length; i++) {
-        let excavator = data.excavators[i];
-
-        if (excavator.schedule){
-            for (let j=0; j<excavator.schedule.length; j++) {
-                schedule.push(
-                    {
-                        id: i, 
-                        name: excavator.name,
-                        site: excavator.schedule[j].location,
-                        start: excavator.schedule[j].start,
-                        end: excavator.schedule[j].end
-                    }
-                )
-            }
-        }
-    }
-    res.send(schedule);
-})
-
+//404 Error
 app.get("*", (req, res) => {
     res.sendStatus(404)
 })
 
-app.listen(8080, () => {console.log("Server running: Port 8080")})
+app.listen(PORT, () => {console.log(`Server Port: ${PORT}`)});
