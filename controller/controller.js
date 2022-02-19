ee856780ae815e0ee7ae81f35ef182c2957778d0
scott88lee@gmail.com <scott88lee@gmail.com>
2022-02-19 13:32:50 +0800

    Refactor

---
+++ controller/controller.js
@@ -0,0 +1,14 @@
+(req, res) => {
+   let data = db.read('db.json');
+   if (data.excavators.length < 6) {
+       data.excavators.push(
+           {
+               id: data.excavators.length + 1,
+               name: req.body.name,
+               description: req.body.description
+           }
+       )
+   }
+   db.write('db.json', data)
+   res.redirect("/excavator.html");
+})
