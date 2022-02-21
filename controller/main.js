ebbc814b769f540242f2d5b02053cdcda38af23b
scott88lee@gmail.com <scott88lee@gmail.com>
2022-02-21 19:18:50 +0800

    sorta working

--- controller/main.js
+++ controller/main.js
@@ -2,6 +2,42 @@ const excavators = require('../model/excavators');
 const dates = require('../helpers/dates');
 const schedule = require('../model/schedule');
 
+// For file upload
+const path = require('path');
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "view/"); // here we specify the destination . in this case i specified the current directory
+    },
+    filename: function (req, file, cb) {
+        console.log(file);
+        cb(null, file.originalname);// here we specify the file saving name . in this case i specified the original file name
+    }
+});
+
+const maxSize = 2 * 1024 * 1024; //In Bytes - num * mega * kilo
+const uploadImage = multer({
+    storage: storage,
+    limits: { fileSize: maxSize },
+    fileFilter: function (req, file, cb) {
+
+        // Set the filetypes, it is optional
+        var filetypes = /jpeg|jpg|png|pdf/;
+        var mimetype = filetypes.test(file.mimetype);
+
+        var extname = filetypes.test(path.extname(
+            file.originalname).toLowerCase());
+
+        if (mimetype && extname) { // Check MIME types on top of FileExt
+            return cb(null, true);
+        } else {
+            cb({ success: false, errMsg: "Invalid File format." }, false)
+        }
+    }
+}).single('image')
+// For file upload
+
 module.exports = {
     getAllExcavators: (req, res) => {
         console.log("CTL > getAllExcavators");
@@ -11,18 +47,35 @@ module.exports = {
 
     addExcavator: (req, res) => {
         console.log("CTL > addExcavator");
-        let excavator = {
-            name: req.body.name,
-            description: req.body.description,
-        }
+        uploadImage (req, res, (err) => {
+            if (err) {
+                if (err.code == "LIMIT_FILE_SIZE") {
+                    return res.send(
+                        { success: false, errMsg: "File size over 2MB." }
+                    )
+                }
+                return res.send(err)
+            }
+            
+            console.log("Request body: " + JSON.stringify(req.body));
+
+            let excavator = {
+                name: req.body.name,
+                description: req.body.description,
+            }
+
+            if (req.file){
+                excavator.imglink = req.file.originalname;
+            }
         
-        let success = excavators.addNew(excavator);
-        if (success) {
-            console.log("Success");
-        } else {
-            console.log("Failed");
-        }
-        res.redirect('/excavator.html');
+            let success = excavators.addNew(excavator);
+            if (success) {
+                console.log("Success");
+            } else {
+                console.log("Failed");
+            }
+            res.redirect('/excavator.html');
+        });
     },
 
     deleteExcavator: (req, res) => {
--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require('express');
-const multer = require('multer');
-const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-const upload = multer({ dest: 'public/' });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,25 +11,9 @@ const ctl = require('./controller/main');
 
 //API Routes
 app.get('/api/excavators', ctl.getAllExcavators);
-// app.post('/api/excavators', ctl.addExcavator);
+app.post('/api/excavators', ctl.addExcavator);
+
 app.get('/api/delete/excavators/:id', ctl.deleteExcavator);
-app.post('/api/excavators', upload.single('myFile'), (req, res) => {
-    console.log("CTL > addExcavator");
-    console.log(req.file)
-    let excavator = {
-        name: req.body.name,
-        description: req.body.description,
-    }
-    
-    const excavators = require('./model/excavators');
-    let success = excavators.addNew(excavator);
-    if (success) {
-        console.log("Success");
-    } else {
-        console.log("Failed");
-    }
-    res.redirect('/excavator.html');
-});
 
 app.get('/api/schedule', ctl.getSchedule);
 app.post('/api/schedule', ctl.addSchedule);
